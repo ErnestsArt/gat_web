774ebefab3c13647e6289c43327f2b11ee01d1d5
Ernests <ernests1012@gmail.com>
2025-02-26 21:18:27 +0200

    Linked js and css file to php files

--- 25022025gat/20240926_PD/web/blog.php
+++ 25022025gat/20240926_PD/web/blog.php
@@ -9,12 +9,12 @@
     <h1>Tiešsaistes dienasgrāmata</h1>
 
     <div>
-        <h2>Biogrāfija</h2>
+        <h2 class="small-heading">Biogrāfija</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores aliquid delectus totam eligendi quibusdam expedita voluptate qui porro adipisci alias laboriosam asperiores recusandae quo sequi odio, doloribus dignissimos perspiciatis maxime eius? Expedita, nesciunt provident? Molestias unde ipsam quasi eius placeat porro, harum excepturi atque nihil natus. Dolores, fugiat repudiandae. Voluptates qui eos molestiae ad aspernatur modi distinctio eum nesciunt quae expedita error hic, amet adipisci sint, provident ducimus sequi minus totam aut. Nemo, qui dolorem. Nemo velit fugiat, quis neque impedit earum laborum esse laboriosam accusamus quas. Vel earum velit ducimus, in quaerat numquam delectus alias tenetur repellendus accusamus magni!</p>
     </div>
 
     <div>
-        <h2>Kapēc es nolēmu kļūt par programmētāju?</h2>
+        <h2 class="small-heading">Kapēc es nolēmu kļūt par programmētāju?</h2>
         <ul>
             <li>ds</li>
             <li>ds</li>
@@ -23,7 +23,7 @@
             <li>xzc</li>
         </ul>
     </div>
-    
+    <link rel="stylesheet" href="styles.css">
 
     <a href="index.php">Next page</a>
 </body>
