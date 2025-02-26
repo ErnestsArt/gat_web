f28237184c8b451a0287e378b22b54b97c0bddaf
Ernests <ernests1012@gmail.com>
2025-02-26 21:05:27 +0200

    Added everything in form , input fields for email, name, level and radio buttons

--- 25022025gat/20240926_PD/web/index.php
+++ 25022025gat/20240926_PD/web/index.php
@@ -6,6 +6,28 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Piesakies webināram!</h1>
+    <form action="" method="post">
+        <label for="name">Name: </label>
+        <input type="text" name="name" id="name">
+        <br>
+        <label for="email">Email: </label>
+        <input type="email" name="email" id="email">
+        <br>
+        <label for="level">Level: </label>
+        <select name="level" id="level">
+            <option value="Advanced">Advanced</option>
+            <option value="Beginner">Beginner</option>
+            <option value="Intermed">Intermed</option>
+        </select>
+        <br>
+        <p>vai vēlies saņemt info?</p>
+        <label for="radio">jā</label>
+        <input type="radio" name="yes" id="yes">
+        <label for="radio">nē</label>
+        <input type="radio" name="yes" id="yes">
+        <br>
+        <input type="submit" value="Nosūtīt">
+    </form>
 </body>
 </html>
