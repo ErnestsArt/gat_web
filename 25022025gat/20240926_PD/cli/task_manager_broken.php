30ed54f24b09c956124a51afb70fd70f417b4e77
Ernests <ernests1012@gmail.com>
2025-02-26 20:48:53 +0200

    added extra feature that lets user input category for task

---
+++ 25022025gat/20240926_PD/cli/task_manager_broken.php
@@ -0,0 +1,119 @@
+<?php
+
+class Task {
+    private $id;
+    private $title;
+    private $description;
+    private $category;
+
+    public function __construct($id, $title, $description, $category) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->description = $description;
+        $this->category = $description;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+
+    public function setCategory($category) {
+        $this->category = $category;
+    }
+
+    public function displayTask() {
+        echo "ID: " . $this->id . "\n";
+        echo "Title: " . $this->title . "\n";
+        echo "Description: " . $this->description . "\n";
+        echo "Category: " . $this->category . "\n";
+        echo "--------------------------\n";
+    }
+}
+
+$tasks = [];
+
+function displayAllTasks($tasks) {
+    if (empty($tasks)) {
+        echo "No tasks available.\n";
+    } else {
+        foreach ($tasks as $task) {
+            $task->displayTask();
+        }
+    }
+}
+
+function createTask(&$tasks) {
+    $title = readline("Enter Task Title: ");
+    $description = readline("Enter Task Description: ");
+    $category = readline("Enter Task Category: ");
+
+    end($tasks);
+    $lastKey = key($tasks);
+    $id = $lastKey + 1;
+
+    $tasks[$id] = new Task($id, $title, $description, $category);
+    echo "Task Created.\n";
+}
+
+function updateTask(&$tasks) {
+    $id = readline("Enter Task ID to Update: ");
+
+    if ($tasks[$id]) {
+        $newTitle = readline("Enter New Title: ");
+        $newDescription = readline("Enter New Description: ");
+        $newCategory = readline("Enter New Category: ");
+
+        $tasks[$id]->setTitle($newTitle);
+        $tasks[$id]->setDescription($newDescription);
+        $tasks[$id]->setCategory($newCategory);
+
+        echo "Task Updated.\n";
+    } else {
+        echo "Task ID not found.\n";
+    }
+}
+
+function deleteTask(&$tasks) {
+    $id = readline("Enter Task id to delete: ");
+    if (isset($tasks[$id])) {
+        unset($tasks[$id]);
+        echo "Task Deleted.\n";
+    } else {
+        echo "Task ID not found.\n";
+    }
+}
+
+while (true) {
+    echo "\nToDo List CLI Application\n";
+    echo "1. Create Task\n";
+    echo "2. View All Tasks\n";
+    echo "3. Update Task\n";
+    echo "4. Delete Task\n";
+    echo "5. Exit\n";
+    echo "Choose an option: ";
+    $choice = trim(fgets(STDIN));
+
+    switch ($choice) {
+        case 1:
+            createTask($tasks);
+            break;
+        case 2:
+            displayAllTasks($tasks);
+            break;
+        case 3:
+            updateTask($tasks);
+            break;
+        case 4:
+            deleteTask($tasks);
+            break;
+        case 5:
+            echo "Exiting the application. Goodbye!\n";
+            exit;
+        default:
+            echo "Invalid option. Please choose again.\n";
+    }
+}
