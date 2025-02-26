cf49d3c03b469d0987b29903eb1b1e139cabf545
Ernests <ernests1012@gmail.com>
2025-02-26 21:09:50 +0200

    Added 2 headers, one has an random text other has an list

--- 25022025gat/20240926_PD/web/blog.php
+++ 25022025gat/20240926_PD/web/blog.php
@@ -6,6 +6,23 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Tiešsaistes dienasgrāmata</h1>
+
+    <div>
+        <h2>Biogrāfija</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores aliquid delectus totam eligendi quibusdam expedita voluptate qui porro adipisci alias laboriosam asperiores recusandae quo sequi odio, doloribus dignissimos perspiciatis maxime eius? Expedita, nesciunt provident? Molestias unde ipsam quasi eius placeat porro, harum excepturi atque nihil natus. Dolores, fugiat repudiandae. Voluptates qui eos molestiae ad aspernatur modi distinctio eum nesciunt quae expedita error hic, amet adipisci sint, provident ducimus sequi minus totam aut. Nemo, qui dolorem. Nemo velit fugiat, quis neque impedit earum laborum esse laboriosam accusamus quas. Vel earum velit ducimus, in quaerat numquam delectus alias tenetur repellendus accusamus magni!</p>
+    </div>
+
+    <div>
+        <h2>Kapēc es nolēmu kļūt par programmētāju?</h2>
+        <ul>
+            <li>ds</li>
+            <li>ds</li>
+            <li>fdgfd</li>
+            <li>xcv</li>
+            <li>xzc</li>
+        </ul>
+    </div>
+
 </body>
 </html>
